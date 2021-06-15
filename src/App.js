75160fe18a456794cb0385df7d4e13d8046f6991
mkrndmb <mkrndmb@gmail.com>
2021-06-15 15:49:56 +0530

    setting the homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+       <HomePage/>
     </div>
   );
 }
